81dbbf1b8234a1660342a171f2e1c55842f876dd
Chukwudolue Divine <codevine2023@gmail.com>
2024-10-01 16:35:05 +0100

    vue in progress

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,12 +1,42 @@
 <script>
 export default {
-  
-}
+  setup() {
+    const name = 'Chukwudolue Divine';
+    const status = 'active';
+    const tasks = ['Football', 'Basketball', 'Golf'];
+
+  }
+  // IN VUE THERE IS OPTIONS API AND COMPOSITIONS API
+  // COMPOSITIONS API
+};
 
 </script>
 <template>
   <h1>
-    Vue Jobs
+    {{ name }}
   </h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else> User is Inactive</p>
+
+
+  <div>
+    <ul>
+      <li v-for="games in tasks" :key="games"> {{ games }}</li>
+    </ul>
+  </div>
+
+  <br>
+
+  <button @click="toggleStatus">Change status</button>
 </template>
 
+<style scoped>
+h1 {
+  color: blue;
+}
+
+p {
+  color: green;
+}
+</style>
---
+++ vue-practice/src/App2.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Chukwudolue Divine',
+      status: 'pending',
+      tasks: ['Football', 'Basketball', 'Golf'],
+      link: 'https://codevine.vercel.app/',
+      golink: 'https://google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending'){
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    }
+  }
+  // IN VUE THERE IS OPTIONS API AND COMPOSITIONS API
+  //  OPTIONS API
+};
+
+</script>
+<template>
+  <h1>
+   {{ name }}
+  </h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else> User is Inactive</p>
+
+
+  <div>
+    <ul>
+      <li v-for="games in tasks" :key="games"> {{ games }}</li>
+    </ul>
+  </div>
+
+  <a v-bind:href="link">Go to my website</a>
+  <a :href="golink">Google</a>
+
+  <br>
+
+  <button @click="toggleStatus">Change status</button>
+</template>
+
+<style scoped>
+h1{
+  color: blue;
+}
+p{
+  color: green;
+}
+</style>
